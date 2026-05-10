fbace7938b12f4270f673a305f373df1b3c67879
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-05-10 18:16:24 +0200

    ✨: enhance Experience component with 3D text and matcap material

--- src/experience.tsx
+++ src/experience.tsx
@@ -1,17 +1,35 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  Center,
+  OrbitControls,
+  Text3D,
+  useMatcapTexture,
+} from "@react-three/drei";
 import { Perf } from "r3f-perf";
 
 export function Experience() {
+  const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
   return (
     <>
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
-      <mesh scale={1.5}>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Center>
+        <Text3D
+          font="/fonts/helvetiker_regular.typeface.json"
+          size={0.5}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          Hello R3F
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </Text3D>
+      </Center>
     </>
   );
 }
